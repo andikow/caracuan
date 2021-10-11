24397fb76762cf84a3df9341cd44122f2ea3ed0e
andikow <katama_andi@yahoo.com>
2021-10-11 21:02:32 +0700

    Pembuatan Struktur Folder & Font Awesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import  './public/assets/fontawesome/css/all.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <i class="fas fa-ad" style={{color:"red"}}></i>
+        <i class="far fa-abacus"></i>
+        <i class="fal fa-user"></i>
+        <i class="fab fa-github-square"></i>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
