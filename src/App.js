c3413f6065be0105e435a08a677d078853295828
andikow <katama_andi@yahoo.com>
2021-10-15 22:54:05 +0700

    Package Baru (React-Bootstrap JS), Ganti Font Monserrat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import  './public/assets/fontawesome/css/all.css';
+import './public/assets/bootstrap/css/bootstrap.css';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function App() {
   return (
@@ -10,6 +12,28 @@ function App() {
         <i class="far fa-abacus"></i>
         <i class="fal fa-user"></i>
         <i class="fab fa-github-square"></i>
+        <button type="button" class="btn btn-primary">Primary</button>
+        <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <Dropdown>
+  <Dropdown.Toggle variant="primary" id="dropdown-basic">
+    Dropdown Button
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
