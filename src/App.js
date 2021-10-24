4d409c699cb6237a1661c2ddb99d24eea331bdd9
vandarinarisca <vandarina.risca@gmail.com>
2021-10-24 20:45:20 +0700

    header creator & member

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { Route, NavLink, HashRouter } from "react-router-dom";
 import './App.css';
 import  './public/assets/fontawesome/css/all.css';
 import './public/assets/bootstrap/css/bootstrap.css';
-import Header from './component/header.js';
+import Headercreator from './component/headercreator.js';
 import Creator from './page/creator.js';
 
 function App() {
   return (
     <HashRouter>
-      <Header />
+      <Headercreator />
     </HashRouter>
   );
 }
