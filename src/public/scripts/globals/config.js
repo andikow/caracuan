c84ef458d4ab5fddb4adbdf5d6f4eb0ca179a5fe
andikow <katama_andi@yahoo.com>
2021-10-24 16:38:07 +0700

    DB Connect & Package Update

--- src/public/scripts/globals/config.js
+++ src/public/scripts/globals/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
-  BASE_URL: 'https://api.themoviedb.org/3/',
+  KEY: 'FAB70C21379142A8B4768AAD0ECF29E1',
+  BASE_URL: 'https://api.stockdio.com/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
 };
