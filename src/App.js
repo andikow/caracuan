310698dd544bcefbc85476505b8c54718753070e
vandarinarisca <vandarina.risca@gmail.com>
2021-12-21 22:13:26 +0700

    Ha cb cek

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import { Route, NavLink, HashRouter } from "react-router-dom";
 import './App.css';
 import  './public/assets/fontawesome/css/all.css';
 import './public/assets/bootstrap/css/bootstrap.css';
-import Header from './component/header.js';
-import HeaderCreator from './component/headercreator.js';
-import Sidebar from './component/sidebar.js';
-import DashboardCreator from './component/dashboardcreator.js';
+import HeaderMember from './component/headermember.js';
+import Sidebarmember from './component/sidebarmember.js';
 
 function App() {
   return (
     <HashRouter>
-      <Header />
+      <HeaderMember />
+      <Sidebarmember />
     </HashRouter>
   );
 }
---
+++ src/component/sidebarmember.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import $ from "jquery";
+import './../public/assets/css/sidebar.css';
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import Akademi from './akademi.js';
+import Mengikuti from './mengikuti.js';
+import Transaksi from './transaksi.js';
+
+class Sidebarmember extends Component {
+
+  render(){
+    return (
+      <>
+      <div class="wrapper">
+              <nav id="sidebar">
+
+                  <ul class="list-unstyled components">
+                    <NavLink to="/">
+                      <li class ="active">
+                          <a href="#">Akademi</a>
+                      </li>
+                    </NavLink>
+                    <NavLink to="/mengikuti">
+                      <li>
+                        <a href="#">Mengikuti</a>
+                      </li>
+                    </NavLink>
+                    <NavLink to="/transaksi">
+                      <li>
+                        <a href="#">Transaksi</a>
+                      </li>
+                    </NavLink>
+                    <li>
+                      <a href="#">Pengaturan</a>
+                    </li>
+                  </ul>
+
+                  <ul class="list-unstyled CTAs">
+                      <li>
+                          <a href="#" class="download">Masuk Sebagai Analis</a>
+                      </li>
+                      <li>
+                          <a href="#" class="article">Keluar</a>
+                      </li>
+                  </ul>
+              </nav>
+              <HashRouter>
+              <Route exact path="/" component={Akademi}/>
+              <Route path="/mengikuti" component={Mengikuti}/>
+              <Route path="/transaksi" component={Transaksi}/>
+              </HashRouter>
+
+          </div>
+
+      </>
+    );
+  }
+}
+
+export default Sidebarmember;
