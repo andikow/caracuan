45056a9c68a994266d769ef8b491b193d36552dd
andikow <katama_andi@yahoo.com>
2022-02-09 22:54:33 +0700

    Layout Pengaturan

--- src/component/pengaturan.js
+++ src/component/pengaturan.js
@@ -6,16 +6,23 @@ class Pengaturan extends Component {
   render() {
     return (
       <>
-      <div class="row">
-        <div class="d-flex col-lg-12">
-        <h2 className="mx-4 my-4 text-primary">Pengaturan</h2>
-        <div class="align-items-center">
-          <img src={Poto} alt="Poto" height="150" style={{borderRadius: "100%", display:'block', marginRight:'auto', marginLeft:'auto'}} />
+      <div class="container">
+        <div class="row">
+          <h2 className="col-12 mx-4 my-4 text-primary">Pengaturan</h2>
         </div>
+        <div class="row">
+          <div class="col-4" style={{backgroundColor:'#455544'}}>
+            <div class="align-items-center">
+              <img src={Poto} alt="Poto" height="150" style={{borderRadius: "100%", display:'block', marginRight:'auto', marginLeft:'auto'}} />
+            </div>
+          </div>
+          <div class="col-8" style={{backgroundColor:'#ccc'}}>
 
-        </div>
-        <div class="d-flex col-lg-12">
+          </div>
 
+          <div class="d-flex col-lg-12">
+
+          </div>
         </div>
       </div>
       </>
